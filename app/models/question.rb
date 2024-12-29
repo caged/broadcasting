53cf29b2bdcb8ddf47e0fb78fbfa8814989724f1
Justin Palmer <encytemedia@gmail.com>
2024-12-29 16:11:54 -0600

    rudimentary radio and checkbox

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,16 @@ class Question < ApplicationRecord
   enum :kind, [ :input_text, :input_select, :input_radio, :input_checkbox ]
 
   validates :label, presence: true
+
+  def human_label
+    label.split('::').first
+  end
+
+  def choices
+    if input_select? || input_radio? || input_checkbox?
+      label.split('::').last.split(',').map { |choice| [choice, choice] }
+    else
+      []
+    end
+  end
 end
